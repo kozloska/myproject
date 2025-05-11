9d40c32efb1d0b6d6b5aea5dbb18051d0e0940c0
Veronika <Veronika@HOME-PC>
2025-05-12 01:34:28 +0300

    запрос на выдачу данных протокола по студенту

--- audio/views/protocol_views.py
+++ audio/views/protocol_views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+
 from ..models import Protocol, Student
 from ..serializers import ProtocolSerializer, UpdateGradeSerializer
 from rest_framework import viewsets, status
@@ -6,4 +8,5 @@ from rest_framework import viewsets, status
 class ProtocolViewSet(viewsets.ModelViewSet):
     queryset = Protocol.objects.all()
     serializer_class = ProtocolSerializer
-
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['ID_Student']
