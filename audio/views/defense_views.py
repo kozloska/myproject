57d49b7fea7f17534f743ee82e1d8259bf0392fd
Veronika <Veronika@HOME-PC>
2025-05-27 20:19:07 -0200

    запросы для получения дат по годам, надо проверять в теории как это будет

--- audio/views/defense_views.py
+++ audio/views/defense_views.py
@@ -1,12 +1,33 @@
+from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, status
-from audio.filters import DefenseScheduleFilter
-from audio.models import DefenseSchedule
-from audio.serializers import DefenseScheduleSerializer, TodayDefenseQuerySerializer
-
+from ..models import DefenseSchedule
+from ..serializers import DefenseScheduleSerializer
+from ..filters import DefenseScheduleFilter
+from django.utils import timezone
+from datetime import datetime
+from django.utils import timezone
+from rest_framework import viewsets
 
 class DefenseViewSet(viewsets.ModelViewSet):
     queryset = DefenseSchedule.objects.all()
     serializer_class = DefenseScheduleSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_class = DefenseScheduleFilter  # Используем наш кастомный фильтр
+    filterset_class = DefenseScheduleFilter
+
+    def get_queryset(self):
+        # Текущая дата и время с учётом часового поюза
+        now = timezone.now()
+        # Начало текущего дня (00:00:00)
+        start_of_day = now.replace(hour=0, minute=0, second=0, microsecond=0)
+        # Конец текущего года (31 декабря 23:59:59)
+        end_of_year = datetime(now.year, 12, 31, 23, 59, 59)
+        end_of_year = timezone.make_aware(end_of_year)
+
+        # Фильтруем записи по диапазону дат
+        queryset = DefenseSchedule.objects.filter(
+            DateTime__gte=start_of_day,
+            DateTime__lte=end_of_year
+        )
+
+        # Применяем дополнительные фильтры
+        return self.filterset_class(self.request.GET, queryset=queryset, request=self.request).qs
