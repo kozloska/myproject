0bdaa40aa926cd42405f1b14a4516901aee524b7
kozloska <veronika_kozlova00@mail.ru>
2026-05-08 13:48:07 +0800

    изменение работы аудиозаписей

--- audio/services/LLMProcessor_service.py
+++ audio/services/LLMProcessor_service.py
@@ -13,14 +13,15 @@ class LLMProcessor:
         self.model = Llama(
             model_path=model_path,
             n_ctx=8192,
-            n_batch=512,
-            n_threads=20,
-            n_threads_batch=20,  
+            n_batch=2048,
+            n_ubatch=2048,
+            n_threads=8,
+            n_threads_batch=8,  
             n_gpu_layers=0,      # 0 = CPU. Для GPU поставьте -1 или количество слоёв
             verbose=True,
             mmap=True,
-            mlock=False,
-            # flash_attn=False,  # Убрано, т.к. у вас CPU (n_gpu_layers=0)
+            mlock=True,
+            # flash_attn=False,  # Убрано, т.к. у вас CPU (n_gpu_layers=0) 
         )
 
     def generate_questions(self, text: str):
