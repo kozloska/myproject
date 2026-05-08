c80a7d185be275525ae1eb5e226f5a9d97c45607
kozloska <veronika_kozlova00@mail.ru>
2026-05-08 14:55:45 +0800

    значение ллм

--- audio/services/LLMProcessor_service.py
+++ audio/services/LLMProcessor_service.py
@@ -13,8 +13,8 @@ class LLMProcessor:
         self.model = Llama(
             model_path=model_path,
             n_ctx=8192,
-            n_batch=2048,
-            n_ubatch=2048,
+            n_batch=8192,
+            n_ubatch=8192,
             n_threads=8,
             n_threads_batch=8,  
             n_gpu_layers=0,      # 0 = CPU. Для GPU поставьте -1 или количество слоёв
